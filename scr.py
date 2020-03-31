0f079ed759e3e732cc774b864f28cb9b09a8c6e4
Yukiprog <nomad048@gmail.com>
2020-04-01 00:54:18 +0900

    add

--- scr.py
+++ scr.py
@@ -3,14 +3,16 @@ import re
 import uuid
 from bs4 import BeautifulSoup
 
-url = "http://oreno-erohon.com/content/70377"
-r = requests.get(url)
+#俺のエロ本URLとコンテンツ番号の取得
+url = "http://oreno-erohon.com/content/"
+contents = "174319";
+r = requests.get(url+contents)
 soup = BeautifulSoup(r.text,'lxml')
-imgs = soup.find_all('img',src=re.compile('.+724x1024.png'))
+imgs = soup.find_all('img',src=re.compile('.+725x1024.jpg'))
 i =0
 for img in imgs:    
         print(img['src'])
         r = requests.get(img['src'])
-        with open(str('./picture/')+str(i)+str('.png'),'wb') as file:
+        with open(str('./picture/'+contents+'/')+str(i)+str('.jpg'),'wb') as file:
                 file.write(r.content)
         i=i+1
